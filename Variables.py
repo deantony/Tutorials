f8992958073c417c094e834862247c3561e8c940
Tony <52037922+deantony@users.noreply.github.com>
2022-06-05 17:17:22 -0600

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,23 @@
+#Declare a variable 
+x = 0
+
+#declare another variable
+y = 5
+
+#output x + y
+print(x + y)
+
+#Change x and y to be strings instead of integers
+
+x = "0"
+y = "5"
+
+#Output x + y again, now that they are strings
+
+print(x + y)
+
+                            #FSTRINGS IN PYTHON
+# declare variable
+name = "Tim"
+#make an f string which is a string with a placeholder for variables
+print(f"Hello, my name is {name}") 
